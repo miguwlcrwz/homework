cca5bfbd0c7a84389f036737c2aa6a73d28a9ca9
Miguel <miguwlcrwz@gmail.com>
2024-09-12 21:55:21 -0300

    first commit

--- exercicio1.c
+++ exercicio1.c
@@ -3,10 +3,10 @@
 int main(void){
 setlocale(LC_ALL, "portuguese"); 
 int numero,sucessor,antecessor;
-printf("escolha um nÃºmero: ");
+printf("escolha um número: ");
 scanf("%d",&numero);
 antecessor = numero - 1;
 sucessor = numero + 1;
-printf("o antecessor desse nÃºmero Ã©: %d e o sucessor Ã© %d",antecessor,sucessor);
+printf("o antecessor desse número é: %d e o sucessor é %d",antecessor,sucessor);
     return 0;
 }
